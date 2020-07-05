bcca708d53ec51cfd2782ff2fda02e02f8e45604
Cruzer-S <yyyynoom@gmail.com>
2020-07-05 21:14:05 +0900

    Chapter 14: Advanced I/O

--- apue.h
+++ apue.h
@@ -55,7 +55,7 @@ static long xsi_version = 0;
 
 #define MAXLINE	4096
 
-#define FILE_MODE	(S_IRUSR | I_IWUSR | S_IRGRP | S_IROTH)
+#define FILE_MODE	(S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH)
 
 #define DIR_MODE	(FILE_MODE | S_IXUSR | S_IXGRP | S_IXOTH)
 
@@ -157,7 +157,6 @@ void err_ret(const char *fmt, ...)
 	err_doit(1, errno, fmt, ap);
 	va_end(ap);
 }
-
 
 void err_sys(const char *fmt, ...)
 {
@@ -388,14 +387,14 @@ int already_running(void)
 	int		fd;
 	char	buf[16];
 
-	fd = open(LOCKFILE, O_RDWR | O_CREAT, LOCKMDOE);
+	fd = open(LOCKFILE, O_RDWR | O_CREAT, LOCKMODE);
 	if (fd < 0) {
 		syslog(LOG_ERR, "can't open %s: %s", LOCKFILE, strerror(errno));
 		exit(1);
 	}
 
 	if (lockfile(fd) < 0) {
-		if (errno == EACCESS || errno == EAGAIN) {
+		if (errno == EACCES || errno == EAGAIN) {
 			close (fd);
 			return 1;
 		}
@@ -408,5 +407,129 @@ int already_running(void)
 	write(fd, buf, strlen(buf) + 1);
 
 	return 0;
+}
+
+void set_fl(int fd, int flags)
+{
+	int		val;
+
+	if ((val = fcntl(fd, F_GETFL, 0)) < 0)
+		err_sys("fcntl F_GETFL error");
+
+	val |= flags;
+
+	if (fcntl(fd, F_SETFL, val) < 0)
+		err_sys("fcntl F_SETFL error");
+}
+
+void clr_fl(int fd, int flags)
+{
+	int		val;
+
+	if ((val = fcntl(fd, F_GETFL, 0)) < 0);
+
+	val &= ~flags;
+
+	if (fcntl(fd, F_SETFL, val) < 0)
+		err_sys("fcntl F_SETFL error");
+}
+
+int lockfile(int fd)
+{
+	struct flock fl;
+
+	fl.l_type = F_WRLCK;
+	fl.l_start = 0;
+	fl.l_whence = SEEK_SET;
+	fl.l_len = 0;
+
+	return (fcntl(fd, F_SETLK, &fl));
+}
+
+int lock_reg(int fd, int cmd, int type, off_t offset, int whence, off_t len)
+{
+	struct flock	lock;
+
+	lock.l_type = type;
+	lock.l_start = offset;
+	lock.l_whence = whence;
+	lock.l_len = len;
+
+	return (fcntl(fd, cmd, &lock));
+}
+
+pid_t lock_test(int fd, int type, off_t offset, int whence, off_t len)
+{
+	struct flock	lock;
+
+	lock.l_type = type;
+	lock.l_start = offset;
+	lock.l_whence = whence;
+	lock.l_len = len;
+
+	if (fcntl(fd, F_GETLK, &lock) < 0)
+		err_sys("fcntl error");
+
+	if (lock.l_type == F_UNLCK)
+		return 0;
+
+	return lock.l_pid;
+}
+
+static volatile sig_atomic_t sigflag;
+
+static sigset_t newmask, oldmask, zeromask;
+
+static void sig_usr(int signo)
+{
+	sigflag = 1;
+}
+
+void TELL_WAIT(void)
+{
+	if (signal(SIGUSR1, sig_usr) == SIG_ERR)
+		err_sys("signal(SIGUSR1) error");
+
+	if (signal(SIGUSR2, sig_usr) == SIG_ERR)
+		err_sys("signal(SIGUSR2) error");
+
+	sigemptyset(&zeromask);
+	sigemptyset(&newmask);
+	sigaddset(&newmask, SIGUSR1);
+	sigaddset(&newmask, SIGUSR2);
+
+	if (sigprocmask(SIG_BLOCK, &newmask, &oldmask) < 0)
+		err_sys("SIG_BLOCK error");
+}
+
+void TELL_PARENT(pid_t pid)
+{
+	kill(pid, SIGUSR2);
+}
+
+void WAIT_PARENT(void)
+{
+	while (sigflag == 0)
+		sigsuspend(&zeromask);
+
+	sigflag = 0;
+
+	if (sigprocmask(SIG_SETMASK, &oldmask, NULL) < 0)
+		err_sys("SIG_SETMASK error");
+}
+
+void TELL_CHILD(pid_t pid)
+{
+	kill(pid, SIGUSR1);
+}
+
+void WAIT_CHILD(void)
+{
+	while (sigflag == 0)
+		sigsuspend(&zeromask);
+	sigflag = 0;
+
+	if (sigprocmask(SIG_SETMASK, &oldmask, NULL) < 0)
+		err_sys("SIG_SETMASK error");
 }
 #endif
