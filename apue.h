1578f1ea3a367e440b889ea6850f39dad2e81e13
Cruzer-S <yyyynoom@gmail.com>
2020-07-07 21:33:22 +0900

    Network IPC: socket

--- apue.h
+++ apue.h
@@ -13,6 +13,7 @@
 #include <sys/stat.h>
 #include <sys/termios.h>
 #include <sys/wait.h>
+#include <sys/socket.h>
 #if defined(MACOS) || !defined(TIOCGWINSZ)
 #include <sys/ioctl.h>
 #endif
@@ -35,6 +36,8 @@
 #include <sys/resource.h>
 
 #include <string.h>
+
+#define MAXSLEEP 128
 
 #define LOCKFILE "/var/run/daemon.pid"
 #define LOCKMODE (S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH)
@@ -532,4 +535,52 @@ void WAIT_CHILD(void)
 	if (sigprocmask(SIG_SETMASK, &oldmask, NULL) < 0)
 		err_sys("SIG_SETMASK error");
 }
+
+int connect_retry(int domain, int type, int protocol, const struct sockaddr *addr, socklen_t alen)
+{
+	int numsec, fd;
+
+	for (numsec = 1; numsec <= MAXSLEEP; numsec <<= 1) {
+		if ((fd = socket(domain, type, protocol)) < 0)
+			return -1;
+
+		if (connect(fd, addr, alen) == 0) {
+			return fd;
+		}
+
+		close (fd);
+
+		if (numsec <= MAXSLEEP / 2)
+			sleep(numsec);
+	}
+
+	return -1;
+}
+
+int initserver(int type, const struct sockaddr *addr, socklen_t alen, int qlen)
+{
+	int fd;
+	int err = 0;
+
+	if ((fd = socket(addr->sa_family, type, 0)) < 0)
+		return -1;
+
+	if (bind(fd, addr, alen) < 0)
+		goto errout;
+
+	if (type == SOCK_STREAM || type == SOCK_SEQPACKET) {
+		if (listen(fd, qlen) < 0)
+			goto errout;
+	}
+
+	return (fd);
+
+errout:
+	err = errno;
+	close(fd);
+	errno = err;
+
+	return -1;
+}
+
 #endif
